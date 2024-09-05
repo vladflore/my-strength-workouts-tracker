5ea7fc874e757ce5446cfc4eaeeb6e7b1c32530d
Vlad Flore <vlad.flore@exb.de>
2024-09-05 12:59:30 +0200

    Add pluto notebook

---
+++ workouts_tracker.jl
@@ -0,0 +1,15 @@
+### A Pluto.jl notebook ###
+# v0.19.46
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 78f9e65c-6b75-11ef-0b5c-75cb28d04bb4
+print("Hello Workouts Tracker")
+
+# ╔═╡ 9d3a686b-d780-47e1-96e0-a9eac0722341
+md"Load workouts csv and plot interesting things"
+
+# ╔═╡ Cell order:
+# ╠═78f9e65c-6b75-11ef-0b5c-75cb28d04bb4
+# ╠═9d3a686b-d780-47e1-96e0-a9eac0722341
