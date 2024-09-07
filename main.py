31bd31c775e27486b5206f26bf42450e0104d0d6
Vlad Flore <flore.vlad@gmail.com>
2024-09-07 18:27:14 +0200

    Refactor

--- main.py
+++ main.py
@@ -11,13 +11,14 @@ start_date_element = document.querySelector("#start-date")
 end_date_element = document.querySelector("#end-date")
 exercises_element = document.querySelector("#exercises")
 
-file_path = "workouts.csv"
-workouts_df = pd.read_csv(file_path)
+workouts_data = "workouts.csv"
 
-workouts_df["date"] = pd.to_datetime(workouts_df["date"], format="%d/%m/%Y")
+df = pd.read_csv(workouts_data)
+unique_exercises = df["exercise"].unique()
+df["date"] = pd.to_datetime(df["date"], format="%d/%m/%Y")
+start_date = df["date"].min()
+end_date = df["date"].max()
 
-start_date = workouts_df["date"].min()
-end_date = workouts_df["date"].max()
 start_date_formatted = start_date.strftime("%d/%m/%Y")
 end_date_formatted = end_date.strftime("%d/%m/%Y")
 start_date_element.innerHTML = f"Start Tracking Date: {start_date_formatted}"
@@ -25,25 +26,28 @@ end_date_element.innerHTML = f"End Tracking Date: {end_date_formatted}"
 
 plt.figure(figsize=(15, 10))
 
-color_maps = ["tab20", "Set3", "Paired"]
-colors = []
 
-for cmap_name in color_maps:
-    cmap = plt.get_cmap(cmap_name)
-    colors.extend([cmap(i) for i in range(cmap.N)])
+def create_colors():
+    color_maps = ["tab20", "Set3", "Paired"]
+    colors = []
+    for cmap_name in color_maps:
+        cmap = plt.get_cmap(cmap_name)
+        colors.extend([cmap(i) for i in range(cmap.N)])
 
-colors = colors[:50]
+    return colors[:50]
 
-unique_exercises = workouts_df["exercise"].unique()
+
+colors = create_colors()
+
 dropdown = document.createElement("select")
 default_option = document.createElement("option")
 default_option.textContent = "Select an exercise"
 default_option.value = ""
 dropdown.appendChild(default_option)
 for exercise in sorted(unique_exercises):
-    max_weight = workouts_df[workouts_df["exercise"] == exercise]["weight"].max()
+    max_weight = df[df["exercise"] == exercise]["weight"].max()
     option = document.createElement("option")
-    option.textContent = f"{exercise} ({max_weight})"
+    option.textContent = f"{exercise} ({max_weight} kg)"
     option.value = exercise
     dropdown.appendChild(option)
 
@@ -58,18 +62,16 @@ dropdown.addEventListener("change", proxy_callback)
 
 exercises_element.appendChild(dropdown)
 
-num_exercises = workouts_df["exercise"].nunique()
+num_exercises = df["exercise"].nunique()
 if num_exercises > len(colors):
     raise ValueError(
         f"Not enough colors! You have {len(colors)}, but need {num_exercises}."
     )
 
-line_style = "-"
-
 exercise_data_list = []
 
 for i, exercise in enumerate(unique_exercises):
-    exercise_data = workouts_df[workouts_df["exercise"] == exercise]
+    exercise_data = df[df["exercise"] == exercise]
     exercise_data_list.append((exercise, exercise_data, colors[i]))
 
 exercise_data_list.sort(key=lambda x: x[0])
@@ -79,7 +81,7 @@ for i, (exercise, exercise_data, color) in enumerate(exercise_data_list):
         exercise_data["date"],
         exercise_data["weight"],
         marker="o",
-        linestyle=line_style,
+        linestyle="-",
         color=color,
         label=f"{i+1}. {exercise}",
     )
@@ -118,7 +120,7 @@ def plot_selected_exercise(selected_exercise):
 
     plt.figure(figsize=(15, 5))
 
-    exercise_data = workouts_df[workouts_df["exercise"] == selected_exercise]
+    exercise_data = df[df["exercise"] == selected_exercise]
 
     plt.plot(
         exercise_data["date"],
@@ -145,7 +147,6 @@ def plot_selected_exercise(selected_exercise):
     plt.xticks(rotation=45)
     plt.gca().axes.get_xaxis().set_visible(True)
 
-    plt.legend(loc="center left", bbox_to_anchor=(1, 0.5))
     plt.tight_layout()
 
     mpl2 = document.querySelector("#mpl2")
